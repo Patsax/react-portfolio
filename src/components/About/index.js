baabda9a8a2d6ea382e0b437e8e23987f807d5a1
Patrick <patasax22@yahoo.com>
2021-12-31 16:25:13 -0500

    bio section section grammar clean up

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,8 +11,8 @@ function About() {
             <div className="bio">
                 <img src={personalPhoto} id="me" className="img-thumbnail" alt="of Patrick Sachleben with blue, collared shirt. He is bald with glasses and a beard." />
                 <div>
-                    <p>My name is Patrick Sachleben. I have worked at my family's Dairy Queen for the last 24 or so years. That path just didn't fulfil me so I decided to enroll in a bootcamp for full stack development. I'm just beginning my journey in this exciting field and I very much look forward to learning and growing my skills.</p>
-                    <p>In my spare time I pursue several forms of entertainment. I like most forms or gaming be they video, board, or even card games. I quite like to read with fantasy and sci-fi being my favorite genres. I also enjoy watching movies such as anything Star Wars, Star Trek, Marvel, etc.  You might call me a huge nerd 😊</p>
+                    <p>My name is Patrick Sachleben. I have worked at my family’s Dairy Queen for the last 24 or so years. That path just didn’t fulfil me, so I decided to enroll in a bootcamp for full stack development. I’m just beginning my journey in this exciting field, and I very much look forward to learning and growing my skills.</p>
+                    <p>In my spare time I pursue several forms of entertainment. I like most forms of gaming be they video, board, or even card games. I quite like to read with fantasy and sci-fi being my favorite genres. I also enjoy watching movies such as anything Star Wars, Star Trek (the two are not mutually exclusive), Marvel, etc. You might call me a huge nerd. 😊</p>
                 </div>
             </div>
         </section>
